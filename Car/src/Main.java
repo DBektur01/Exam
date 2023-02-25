64a173e17671361c709407f6eccb51472f9ee27f
Bektur Duyshenbek uulu <84037263+DBektur01@users.noreply.github.com>
2023-02-25 11:54:45 +0600

    Update Main.java

--- Car/src/Main.java
+++ Car/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Car bmw = new Car("BMW", LocalDate.of(2022,5,2),new BigDecimal(120000),TypeCar.SEDAN);
         System.out.println(bmw);
        bmw.determinationYearOfManufacture(bmw.getDateOfMade());
-       bmw.bodyTypeIdentification(bmw.getTypeCar());
+        System.out.println(bmw.bodyTypeIdentification(bmw.getTypeCar()));
 
         System.out.println("-------------------------------------");
 
@@ -14,7 +14,7 @@ public class Main {
         Car audi = new Car("AUDI", LocalDate.of(2021,5,2),new BigDecimal(120000),TypeCar.STATION_WAGON);
         System.out.println(audi);
         audi.determinationYearOfManufacture(audi.getDateOfMade());
-        audi.bodyTypeIdentification(audi.getTypeCar());
+         System.out.println(audi.bodyTypeIdentification(audi.getTypeCar()));
 
         System.out.println("-------------------------------------");
 
@@ -22,18 +22,18 @@ public class Main {
         Car  toyota = new Car("TOYOTA", LocalDate.of(2020,11,12),new BigDecimal(150000),TypeCar.SUV);
         System.out.println(toyota);
         toyota.determinationYearOfManufacture(toyota.getDateOfMade());
-        toyota.bodyTypeIdentification(toyota.getTypeCar());
+        System.out.println(toyota.bodyTypeIdentification(toyota.getTypeCar()));
 
         Car honda = new Car("Honda", LocalDate.of(2017,5,2),new BigDecimal(120000),TypeCar.HATCHBACK);
         System.out.println(honda);
         bmw.determinationYearOfManufacture(honda.getDateOfMade());
-        bmw.bodyTypeIdentification(bmw.getTypeCar());
+        System.out.println(honda.bodyTypeIdentification(honda.getTypeCar()));
 
         System.out.println("-------------------------------------");
         Car mazda = new Car("Mazda", LocalDate.of(2015,5,2),new BigDecimal(120000),TypeCar.CROSSOVER);
         System.out.println(mazda);
         bmw.determinationYearOfManufacture(mazda.getDateOfMade());
-        bmw.bodyTypeIdentification(mazda.getTypeCar());
+       System.out.println(bmw.bodyTypeIdentification(mazda.getTypeCar()));
 
 
 
