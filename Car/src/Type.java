76fc602044196cfdef4ed8f797c18d449f0c6b0e
Bektur Duyshenbek uulu <84037263+DBektur01@users.noreply.github.com>
2023-02-25 11:41:27 +0600

    Update Type.java

--- Car/src/Type.java
+++ Car/src/Type.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public interface Type {
 
-    void bodyTypeIdentification(TypeCar typeCar);
+    String bodyTypeIdentification(TypeCar typeCar);
 
     void determinationYearOfManufacture(LocalDate yearOfManufacture);
 }
